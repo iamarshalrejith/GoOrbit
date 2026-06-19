3f0c24a39c25cd4b5e840331208814b090f5f36c
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-06-19 23:42:01 +0530

    feat(p2p): define peer and transport interfaces

---
+++ p2p/transport.go
@@ -0,0 +1,13 @@
+package p2p
+
+// Peer is a interface that represents the remote node
+type Peer interface{
+
+}
+
+// Transport is anything that handles the communication
+// between the nodes in the network.
+// This can be in the form of TCP,UDP, websockets, ...
+type Transport interface{
+
+}
