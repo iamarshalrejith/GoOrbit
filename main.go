2c8367b11359ed28c2bee54d01e2225c63399fbc
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-06-24 20:42:59 +0530

    feat(node): start TCP transport and add basic transport tests

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-
+import (
+	"log"
+	"github.com/iamarshalrejith/GoOrbit/p2p"
+)
 
 func main() {
-	
+	tr := p2p.NewTCPTransport(":3000")
+	if err := tr.ListenAndAccept(); err!= nil{
+		log.Fatal(err)
+	}
+	select {}
 }
